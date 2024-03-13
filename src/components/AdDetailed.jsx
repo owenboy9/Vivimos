8aff1a1ba700cd28f9992d447c51cb63439a7a27
fantalec <mikaelfant@gmail.com>
2024-03-13 14:51:45 +0100

    jobbat lite med detaljerad annonsvisningssida

--- src/components/AdDetailed.jsx
+++ src/components/AdDetailed.jsx
@@ -23,16 +23,43 @@ function AdDetailed() {
   const handleOfferButton = () => {
     navigate(`/ad/${id}/bid`)
   }
+  const pronomenLista = {
+    man: "Han",
+    kvinna: "Hon",
+    annat: "Hen"
+  };
+  const pronomen = pronomenLista[ad.kön.toLowerCase()];
+
+  const pets = ["Hund", "Katt", "Fågel", "Häst"];
+  let petsList = [];
+
+  for (let pet of pets) {
+    if (ad[pet]) {
+      petsList.push(pet.toLowerCase());
+    }
+  }
+
+  let petsString = petsList.join(", ");
+
+  let petsSentence = ad.husdjur === "" ? `${pronomen} har inga husdjur.` : `${pronomen} har ${petsString}.`;
+
+  if (ad.Annat) {
+    petsSentence += ` ${pronomen} har även ${ad.Annat.toLowerCase()}.`;
+  }
+
 
   return (
     <div>
       <h1>{ad.rubrik}</h1>
       <p>{ad.presentation}</p>
-      <p>En {ad.ålder} år gammal {ad.kön.toLowerCase()}, från {ad.län} <br/> som
-        är {ad.sysselsättning.toLowerCase()} och bor i {ad.stad.toLowerCase()}.</p>
-        {ad.bids ? (<p>Antal bud: {ad.bids.length}</p>) : null }
-        <button onClick={handleOfferButton}>Lägg ett bud</button>
+      <p>Annonsören är en {ad.ålder} år gammal {ad.kön.toLowerCase()}, från {ad.län} län. <br/> {pronomen} är {ad.sysselsättning.toLowerCase()} och bor i {ad.stad.toLowerCase()}. </p>
+      <p>{petsSentence}</p>
+
+      <p>Denna annons är aktiv till och med {ad.enddate}.</p>
+      {ad.bids ? (<p>Antal bud: {ad.bids.length}</p>) : null}
+      <button onClick={handleOfferButton}>Lägg ett bud</button>
     </div>
   );
 }
+
 export default AdDetailed;
